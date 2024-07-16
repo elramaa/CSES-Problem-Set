54cf072d6cdfca5707850b75f6ee130399030129
elramaa <ramaelmaulanaagung@gmail.com>
2024-07-16 23:58:08 +0700

    updating Apartments.cpp, but problem still not solved

--- Sorting and Searching/Apartments.cpp
+++ Sorting and Searching/Apartments.cpp
@@ -2,17 +2,20 @@
 using namespace std;
 
 int main(){
-	int n, m, k, count;
+	int n, m, k;
+	int count = 0;
 	cin >> n >> m >> k;
 	int a[n], b[m];
+	for(int i = 0; i < n; ++i) cin >> a[i];
+	for(int i = 0; i < m; ++i) cin >> b[i];
 	for(int i = 0; i < n; ++i){
-		cin >> a[i];
-	}
-	for(int i = 0; i < m; ++i){
-		cin >> b[i];
-	}
-	for(auto x : a){
-		if()
+		for(int j = 0; j < m; j++){
+			if((b[j] - k) <= a[i] && a[i] <= (b[j] + k)){
+				b[j] = -1000;
+				count++;
+				break;
+			}
+		}
 	}
 	cout << count;
 	return 0;
